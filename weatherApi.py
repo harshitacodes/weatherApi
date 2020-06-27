74c63b3174e7ba9b5e316c0511cf2d9794a96a78
harshita <harshitacodes>
2020-06-27 09:17:44 +0530

    updating files

--- weatherApi.py
+++ weatherApi.py
@@ -1,12 +1,42 @@
+# import requests
+# import json
+
+
+# city_name = input("enter the name of city: ")
+# try:
+#     weatherapi_url = "http://api.openweathermap.org/data/2.5/weather?q=" + str(city_name) + "&appid=05964be4af2fc2a2b7525c680828da7c&units=metric"
+#     res_text = requests.get(weatherapi_url)
+#     weather_data = json.loads(res_text)
+#     temperature = weather_data["main"]["temp"]
+#     print(temperature)
+    
+# #     # print(res_text)
+# # except:
+# #     print("internet is not working")
+# # try:
+# #     weather_data = json.loads(res_text)
+# #     temperature = weather_data["main"]["temp"]
+# #     print(temperature)
+    
+# except:
+#     print("incorrect city name")
+
+
+ 
 import requests
 import json
 
 city_name = input("enter the name of city: ")
-weatherapi_url = "http://api.openweathermap.org/data/2.5/weather?q=" + str(city_name) + "&appid=05964be4af2fc2a2b7525c680828da7c&units=metric"
-def apiCall(url):
-    res = requests.get(url)
-    return res.text
-res_text = apiCall(weatherapi_url)
-weather_data = json.loads(res_text)
-dict_data = weather_data["main"]["temp"]
-print(dict_data)
+try:
+    weatherapi_url = "http://api.openweathermap.org/data/2.5/weather?q=" + str(city_name) + "&appid=05964be4af2fc2a2b7525c680828da7c&units=metric"
+    res = requests.get(weatherapi_url)
+    r_text = res.text
+    weather_data = json.loads(r_text)
+    dict_data = weather_data["main"]["temp"]
+    print(dict_data)
+except KeyError:
+    print("incorrect city name")
+
+except:
+    print("internet is not working")
+
