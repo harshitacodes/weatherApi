b7b16d94d10392a129367965bdc1f276bb9d2f0d
harshitacodes <harhsitagupta608@gmail.com>
2020-07-03 13:13:44 +0530

    adding files

--- weatherApi.py
+++ weatherApi.py
@@ -1,28 +1,3 @@
-# import requests
-# import json
-
-
-# city_name = input("enter the name of city: ")
-# try:
-#     weatherapi_url = "http://api.openweathermap.org/data/2.5/weather?q=" + str(city_name) + "&appid=05964be4af2fc2a2b7525c680828da7c&units=metric"
-#     res_text = requests.get(weatherapi_url)
-#     weather_data = json.loads(res_text)
-#     temperature = weather_data["main"]["temp"]
-#     print(temperature)
-    
-# #     # print(res_text)
-# # except:
-# #     print("internet is not working")
-# # try:
-# #     weather_data = json.loads(res_text)
-# #     temperature = weather_data["main"]["temp"]
-# #     print(temperature)
-    
-# except:
-#     print("incorrect city name")
-
-
- 
 import requests
 import json
 
